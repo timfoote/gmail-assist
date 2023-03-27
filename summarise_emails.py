a3d90e0e692dbed70567bcc15d96fe364e029a11
Tim Foote <timfoote@gmail.com>
2023-03-27 21:12:11 +1000

    working copy

--- summarise_emails.py
+++ summarise_emails.py
@@ -2,6 +2,8 @@ import os
 import base64
 import pickle
 import openai
+import time
+import re
 from google.oauth2 import service_account
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -30,7 +32,7 @@ def authenticate_openai_api():
         openai.api_key = key_file.read().strip()
 
 def get_emails_from_sender(service, sender_email, user_id='me'):
-    query = f"from:{sender_email}"
+    query = f"from:{sender_email} after:2023/02/01 before:2023/02/14"
     response = service.users().messages().list(userId=user_id, q=query).execute()
     messages = response.get('messages', [])
     emails = []
@@ -48,7 +50,7 @@ def get_emails_from_sender(service, sender_email, user_id='me'):
             'from': None,
             'to': None,
             'date': None,
-            'content': None
+            'content': '' # Initialize content to an empty string
         }
 
         for header in headers:
@@ -67,34 +69,109 @@ def get_emails_from_sender(service, sender_email, user_id='me'):
                 if part['mimeType'] == 'text/plain':
                     email_data['content'] = base64.urlsafe_b64decode(part['body']['data']).decode('utf-8')
                     break
-            emails.append(email_data)
-        return emails
+        else:
+            email_data['content'] = "This email does not contain any content."
 
-def summarize_email_content(content):
-    prompt = f"Please summarize the following email content:\n\n{content}\n\nSummary:"
+        emails.append(email_data)
+
+    return emails
+
+
+def remove_inline_replies(content):
+    # Remove quoted text starting with ">"
+    content = re.sub(r'^>.*$', '', content, flags=re.MULTILINE)
+
+    # Remove inline replies starting with "On...wrote:"
+    content = re.sub(r'On\s.*\swrote:.*', '', content, flags=re.MULTILINE)
+
+    return content
+
+
+# def summarize_and_detect_tone(content):
+#     prompt = (f"Please summarize the following email content and detect its tone:\n\n"
+#               f"{content}\n\n"
+#               f"==Summary==\n{{summary}}"
+#               f"==Tone==\n{{tone}}")
+
+#     response = openai.Completion.create(
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         max_tokens=500,
+#         n=1,
+#         stop=None,
+#         temperature=0.5,
+#     )
+
+#     result_text = response.choices[0].text.strip()
+#     matches = re.findall(r'==Summary==\n(.*?)\n==Tone==\n(.*?)', result_text, re.DOTALL)
+
+#     if matches:
+#         summary, tone = matches[0]
+#         return summary.strip(), tone.strip()
+#     else:
+#         return "Summary not found", "Tone not found"
+
+# def summarize_and_detect_tone(content):
+#     prompt = (f"Please summarize the following email content and detect its tone:\n\n"
+#               f"{content}\n\n"
+#               f"==Summary==\n{{summary}}"
+#               f"==Tone==\n{{tone}}")
+
+#     response = openai.Completion.create(
+#         engine="text-davinci-003",
+#         prompt=prompt,
+#         max_tokens=1000,
+#         n=1,
+#         stop=None,
+#         temperature=0.5,
+#     )
+
+#     result_text = response.choices[0].text.strip()
+#     result_lines = result_text.splitlines()
+
+#     summary = "Summary not found"
+#     tone = "Tone not found"
+
+#     for i, line in enumerate(result_lines):
+#         if line.startswith("==Summary=="):
+#             summary = result_lines[i + 1].strip()
+#         if line.startswith("==Tone=="):
+#             tone = result_lines[i + 1].strip()
+
+#     return summary, tone
+
+def summarize_and_detect_tone(content):
+    if len(content) < 50:  # Adjust this value as needed
+        return "Content is too short to summarize.", "Not applicable"
+    
+    prompt = (f"Please summarize the following email content and detect its tone:\n\n"
+              f"{content}\n\n"
+              f"Summary: {{summary}}\n"
+              f"Tone: {{tone}}")
+
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=50,
+        max_tokens=1000,
         n=1,
         stop=None,
         temperature=0.5,
     )
 
-    return response.choices[0].text.strip()
+    result_text = response.choices[0].text.strip()
+    result_lines = result_text.splitlines()
 
-def detect_email_tone(content):
-    prompt = f"Please detect the tone of the following email content:\n\n{content}\n\nTone:"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=20,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
+    summary = "Summary not found"
+    tone = "Tone not found"
 
-    return response.choices[0].text.strip()
+    for line in result_lines:
+        if line.startswith("Summary:"):
+            summary = line[len("Summary:"):].strip()
+        if line.startswith("Tone:"):
+            tone = line[len("Tone:"):].strip()
+
+    return summary, tone
+
 
 def main():
     # Authenticate with the Gmail and OpenAI APIs
@@ -102,26 +179,27 @@ def main():
     authenticate_openai_api()
 
     # Replace 'sender@example.com' with the sender's email address you want to filter
-    sender_email = 'JasonMM@intellichoice.com.au'
+    sender_email = 'terri.howarth@gmail.com'
     emails = get_emails_from_sender(gmail_service, sender_email)
     if emails:
-    # Create a log file to store the email data along with the context and tone attributes
-        with open("email_log.txt", "w") as log_file:
-            for email in emails:
-                content = email['content']
+        for email in emails:
+            content = email['content']
+            if content:
+                cleaned_content = remove_inline_replies(content)
+                summary, tone = summarize_and_detect_tone(cleaned_content)
+                time.sleep(3)  # Add a delay between combined API calls
 
-                summary = summarize_email_content(content)
-                tone = detect_email_tone(content)
-
-                log_entry = f"Subject: {email['subject']}\n" \
-                            f"From: {email['from']}\n" \
-                            f"To: {email['to']}\n" \
-                            f"Date: {email['date']}\n" \
-                            f"Summary: {summary}\n" \
-                            f"Tone: {tone}\n\n" \
-                            f"{'-'*80}\n\n"
-                
-                log_file.write(log_entry)
+                log_entry = (f"Subject: {email['subject']}\n"
+                             f"From: {email['from']}\n"
+                             f"To: {email['to']}\n"
+                             f"Date: {email['date']}\n"
+                             f"Summary: {summary}\n"
+                             f"Tone: {tone}\n\n"
+                             f"{'-'*80}\n\n")
+                    
+                with open("email_log.txt", "a") as log_file:
+                    log_file.write(log_entry)
+                    log_file.flush()
 
         print("Email log has been created.")
     else:
